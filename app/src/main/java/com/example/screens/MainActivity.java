d51830a5fba5433fa011325cc39cff0980282c38
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-04-21 22:24:28 +0200

    Added clickable button to MainActivity
    Added clickable button to the MainActivity along with a updated view that shows some random text.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Try to find button by ID
+        Button btn = findViewById(R.id.presser);
+        // Create a on-click listener
+        btn.setOnClickListener(new View.OnClickListener() {
+            // Implement functionality for clicking on btn
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
